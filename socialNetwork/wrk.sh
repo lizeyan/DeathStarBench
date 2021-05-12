179c2014201e11809f675ae597129491ba96a971
lizeyan <li_zeyan@icloud.com>
2021-05-12 17:55:22 +0800

    change wrk.sh to dead loop

--- socialNetwork/wrk.sh
+++ socialNetwork/wrk.sh
@@ -2,4 +2,6 @@
 cd $(dirname $0)/
 cd wrk2
 echo CWD=${PWD}
-./wrk -D exp -t 8 -c 100 -R 100 -d 1h -L -p -s ./scripts/social-network/mixed-workload.lua http://$(kubectl -n social-network get svc nginx-thrift -o json | jq -r ".spec.clusterIP"):8080
+while [ 1 == 1 ]; do
+  ./wrk -D exp -t 8 -c 100 -R 100 -d 1h -L -p -s ./scripts/social-network/mixed-workload.lua http://$(kubectl -n social-network get svc nginx-thrift -o json | jq -r ".spec.clusterIP"):8080
+done
