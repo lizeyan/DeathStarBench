3ead40e968a31d1fa541b98d84b59456cc1654af
lizeyan <li_zeyan@icloud.com>
2021-05-13 15:48:37 +0800

    debug MM. Found that there are duplicated titles in the movie data.

--- mediaMicroservices/wrk.sh
+++ mediaMicroservices/wrk.sh
@@ -3,5 +3,5 @@ cd $(dirname $0)/
 cd wrk2
 echo CWD=${PWD}
 while [ 1 == 1 ]; do
-  ./wrk -D exp -t 8 -c 100 -d 1h -L -s ./scripts/media-microservices/compose-review.lua http://$(kubectl -n media-microsvc get svc nginx-web-server -o json | jq -r ".spec.clusterIP"):8080/wrk2-api/review/compose -R 100
+  ./wrk -D exp -t 8 -c 100 -d 1h -L -s ./scripts/media-microservices/compose-review.lua http://$(kubectl -n media-microsvc get svc nginx-web-server -o json | jq -r ".spec.clusterIP"):8080/wrk2-api/review/compose -R 10
 done
--- mediaMicroservices/wrk2/scripts/media-microservices/compose-review.lua
+++ mediaMicroservices/wrk2/scripts/media-microservices/compose-review.lua
@@ -1020,6 +1020,9 @@ function string.random(length)
   end
 end
 
+
+tmp_body = ""
+
 request = function()
   local movie_index = math.random(1000)
   local user_index = math.random(1000)
@@ -1034,9 +1037,17 @@ request = function()
   local headers = {}
   local body = "username=" .. username .. "&password=" .. password .. "&title=" ..
                   title .. "&rating=" .. rating .. "&text=" .. text
+  tmp_body = body
   headers["Content-Type"] = "application/x-www-form-urlencoded"
 
   return wrk.format(method, path, headers, body)
+end
+
+
+response = function(status, header, body)
+    if status ~= 200 then
+        print("Failed request. Status=" .. status .. " Body=" .. tmp_body)
+    end
 end
 
 function urlEncode(s)
