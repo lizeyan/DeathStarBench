f74363c800139280c761a7bddbb295a33ab515c9
lizeyan <li_zeyan@icloud.com>
2021-05-13 11:00:38 +0800

    Got unknown error

---
+++ mediaMicroservices/k8s-yaml/scripts/build-docker-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+
+EXEC=docker
+
+REPO="docker.peidan.me/"
+USER="lizytalk"
+
+TAG="latest"
+
+IMAGE=media-microservices
+
+# ENTER THE ROOT FOLDER
+cd ../
+ROOT_FOLDER=$(pwd)
+echo ROOT_FOLDER=${ROOT_FOLDER}
+
+cd $ROOT_FOLDER
+$EXEC build -t "$REPO$USER"/"$IMAGE":"$TAG" -f Dockerfile .
+$EXEC push "$REPO$USER"/"$IMAGE":"$TAG"
+cd $ROOT_FOLDER
+
+
+cd - >/dev/null
