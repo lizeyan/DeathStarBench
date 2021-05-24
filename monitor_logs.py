fd7ec448e0481bd18a2bcb28f7744f8c27cdc608
lizeyan <li_zeyan@icloud.com>
2021-05-24 11:07:32 +0800

    monitor logs

--- monitor_logs.py
+++ monitor_logs.py
@@ -3,6 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from subprocess import Popen, PIPE, getoutput
 from typing import List, Union
+import click
 
 
 def get_services(namespace: str) -> List[str]:
@@ -25,15 +26,18 @@ def errors_in_logs(namespace: str, svc: str) -> Union[None, str]:
         )) > 0
     ]
     if len(error_logs) > 0:
-        error_logs = '\n'.join(error_logs[:10])
+        error_logs = '\n'.join(error_logs[-10:])
         return f"{namespace} svc/{svc}:\n{error_logs}"
     else:
         return None
 
 
-def main():
-    namespace = 'media-microsvc'
-    email = "li_zeyan@icloud.com"
+@click.command("Check error keywords in logs")
+@click.option("--disable-email", is_flag=True, default=False)
+@click.option("--email-address", type=str, default="li_zeyan@icloud.com")
+@click.argument("namespace", type=click.Choice(['social-network', 'media-microsvc'], case_sensitive=False))
+def main(namespace: str, disable_email: bool, email_address: str):
+    email = email_address
     services = get_services(namespace)
     with ThreadPoolExecutor(max_workers=8) as pool:
         errors_logs = "\n\n================================\n\n".join(filter(
@@ -41,22 +45,24 @@ def main():
             pool.map(lambda _: errors_in_logs(namespace, _), services)
         ))
     if errors_logs != "":
-        print(f"send to {email}")
-        cmd = f'mail ' \
-              f'-a "From: {namespace} monitor <{namespace}-monitor@peidan.me>" ' \
-              f' -s "{datetime.now().isoformat()} Error detected in logs in {namespace}" ' \
-              f' --content-type "text/plain" ' \
-              f' {email}'
-        send_mail = Popen(
-            cmd,
-            shell=True,
-            stdin=PIPE
-        )
-        _message = errors_logs
-        send_mail.communicate(
-            _message.encode('utf-8')
-        )
-        send_mail.poll()
+        if not disable_email:
+            print(f"send to {email}")
+            cmd = f'mail ' \
+                  f'-a "From: {namespace} monitor <{namespace}-monitor@peidan.me>" ' \
+                  f' -s "{datetime.now().isoformat()} Error detected in logs in {namespace}" ' \
+                  f' --content-type "text/plain" ' \
+                  f' {email}'
+            send_mail = Popen(
+                cmd,
+                shell=True,
+                stdin=PIPE
+            )
+            _message = errors_logs
+            send_mail.communicate(
+                _message.encode('utf-8')
+            )
+            send_mail.poll()
+        print(errors_logs)
     else:
         print(f"{datetime.now().isoformat()} no error detected")
 
