dbded410572e66f1d96232b2dfb5509d8927a94e
lizeyan <li_zeyan@icloud.com>
2021-05-24 17:56:47 +0800

    update kibana setting

--- socialNetwork/wrk.sh
+++ socialNetwork/wrk.sh
@@ -3,5 +3,5 @@ cd $(dirname $0)/
 cd wrk2
 echo CWD=${PWD}
 while [ 1 == 1 ]; do
-  ./wrk -D exp -t 1 -c 100 -R 1 -d 1h -L -p -s ./scripts/social-network/mixed-workload.lua http://$(kubectl -n social-network get svc nginx-thrift -o json | jq -r ".spec.clusterIP"):8080
+  ./wrk -D exp -t 8 -c 100 -R 100 -d 1h -L -p -s ./scripts/social-network/mixed-workload.lua http://$(kubectl -n social-network get svc nginx-thrift -o json | jq -r ".spec.clusterIP"):8080
 done
